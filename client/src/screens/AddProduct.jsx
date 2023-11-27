1d366718f8de105fea8af5e0e28611d40fd4760c
Adesh Sookwa <ksookwa@outlook.com>
2023-11-26 23:17:10 -0500

    replaced useHistory with useNavigate

--- client/src/screens/AddProduct.jsx
+++ client/src/screens/AddProduct.jsx
@@ -1,13 +1,17 @@
 import axios from "axios";
 import React, { useState } from "react";
 import { Button, Container, Form } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 
 const AddProduct = ({ history }) => {
   const [title, setTitle] = useState("");
   const [price, setPrice] = useState(0);
   const [description, setDescription] = useState("");
+  const navigate = useNavigate();
 
-  const addProductHandler = async () => {
+  const addProductHandler = async (e) => {
+    e.preventDefault();
+
     console.log("Before request");
     const data = {
       title: title,
@@ -16,6 +20,8 @@ const AddProduct = ({ history }) => {
       published: true,
     };
     const response = await axios.post("/api/products/addProduct", data);
+
+    navigate("/products");
   };
 
   return (
@@ -23,7 +29,7 @@ const AddProduct = ({ history }) => {
       <Container className="mt-5 p-2">
         <h1> Add Product</h1>
         <hr />
-        <Form>
+        <Form onSubmit={addProductHandler}>
           <Form.Group className="mb-3" controlId="title">
             <Form.Label>Title</Form.Label>
             <Form.Control
@@ -51,7 +57,7 @@ const AddProduct = ({ history }) => {
               onChange={(e) => setDescription(e.target.value)}
             />
           </Form.Group>
-          <Button onClick={addProductHandler}>Add Product</Button>
+          <Button type="submit">Add Product</Button>
         </Form>
       </Container>
     </>
